db72ef5612063ee46926dbd8037b56715685aaaa
XiaolongLiuLiuLiu <larryravenclaw@gmail.com>
2016-11-13 16:46:41 -0500

    The practice in datatype

--- Ex_Files_Visual_Basic_EssT/Exercise Files/02_GettingStarted/CodeRunner/CodeRunner/MainWindow.xaml.vb
+++ Ex_Files_Visual_Basic_EssT/Exercise Files/02_GettingStarted/CodeRunner/CodeRunner/MainWindow.xaml.vb
@@ -2,7 +2,17 @@
 
     Sub RunCode(sender As Object, e As RoutedEventArgs)
         'Add code here
-        Output(txtInput.Text)
+        Dim o As Object = New Object()
+        If IsNothing(o) Then
+            Output("No object exists")
+        Else
+            Output("Before assignment to value: " + o.GetType().ToString())
+        End If
+
+        o = 1
+        Output("As Number : " + o.GetType().ToString())
+        o = "Green"
+        Output("As String : " + o.GetType().ToString())
 
     End Sub
 
